e6949235652d989747260e9b50d03869f9315c67
Ayush Kedia <ayushkedia1990@gmail.com>
2023-10-09 20:02:21 +0530

    first commit

---
+++ simpleblog/src/test/java/com/cypherpoll0/simpleblog/SimpleblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypherpoll0.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
